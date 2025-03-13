a4acee75d1819f8082940094e77395dbd76eb893
mtlccl <matheus.carvalho.lemes@hotmail.com>
2025-03-13 13:29:53 -0300

    front

---
+++ src/main/java/br/com/matheuslemes/front_gestao_vagas/modules/candidate/service/CreateCandidateService.java
@@ -0,0 +1,33 @@
+package br.com.matheuslemes.front_gestao_vagas.modules.candidate.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import br.com.matheuslemes.front_gestao_vagas.modules.candidate.dto.CreateCandidateDTO;
+
+
+@Service
+public class CreateCandidateService {
+
+    @Value("${host.api.gestao.vagas}")
+    private String hostAPIGestaoVagas;
+    
+    public void execute(CreateCandidateDTO createCandidateDTO) {
+        
+            RestTemplate rt = new RestTemplate();
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+
+            HttpEntity<CreateCandidateDTO> request = new HttpEntity<>(createCandidateDTO ,headers);
+
+            var url = hostAPIGestaoVagas.concat("/candidate/");
+
+            var result = rt.postForObject(url, request, String.class);
+            System.out.println(result);
+    }
+}
---
+++ src/main/java/br/com/matheuslemes/front_gestao_vagas/modules/company/service/CreateCompanyService.java
@@ -0,0 +1,33 @@
+package br.com.matheuslemes.front_gestao_vagas.modules.company.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import br.com.matheuslemes.front_gestao_vagas.modules.company.dto.CreateCompanyDTO;
+
+
+@Service
+public class CreateCompanyService {
+
+    @Value("${host.api.gestao.vagas}")
+    private String hostAPIGestaoVagas;
+
+     public String execute(CreateCompanyDTO createCompanyDTO) {
+        
+            RestTemplate rt = new RestTemplate();
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+
+            HttpEntity<CreateCompanyDTO> request = new HttpEntity<>(createCompanyDTO ,headers);
+
+            var url = hostAPIGestaoVagas.concat("/company/");
+
+            return rt.postForObject(url, request, String.class);
+            
+    }
+}
